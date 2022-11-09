f54b8cc6670a49f8df00db144ce6593cfd8b9928
Liia Zubova <vardlitmo@gmail.com>
2022-11-09 18:14:26 +0300

    add second part

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
 const button = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-const colors = [
-  '#ff5733',
-  '#f9ff33',
-  '#3371ff',
-  'green',
-  'red',
-  'rgb(85, 255, 51, 0.7',
+// const colors = [
+//   '#ff5733',
+//   '#f9ff33',
+//   '#3371ff',
+//   'green',
+//   'red',
+//   'rgb(85, 255, 51, 0.7',
+// ];
+
+const hex = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
 ];
 
 button.addEventListener('click', () => {
-//   console.log('click');
-//   console.log (getRandomColor());
-let hexColor = colors[getRandomColor()];
-document.body.style.backgroundColor = hexColor;
-color.textContent = hexColor;
+  let hexColor = generateHex();
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
 });
 
-function getRandomColor() {
-  return Math.floor(Math.random() * colors.length);
+function generateHex() {
+  let hexColor = '#';
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomColor()];
+  }
+
+return hexColor;
 }
 
+function getRandomColor() {
+  return Math.floor(Math.random() * hex.length);
+}
